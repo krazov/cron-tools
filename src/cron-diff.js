6fc0f03a5f0ac4e25ccee9accda68bf5800ad69a
Sebastian Cholewa <sebastian.cholewa@gig.com>
2020-10-07 20:17:52 +0200

    cron-diff
    * Handling all the combinations of minutes and hours
    * Tests for the above

--- src/cron-diff.js
+++ src/cron-diff.js
@@ -17,6 +17,7 @@ function cronDiff(cron1, cron2) {
     if (count != count2) throw Error('Mismatched crons.');
 
     const response = [];
+    let shouldReduce = null;
 
     for (const label of labels) {
         const interval1 = parsed1[label];
@@ -25,15 +26,17 @@ function cronDiff(cron1, cron2) {
         // covers the same values, including *
         if (interval1 == interval2) continue;
 
-        const diff = interval2 - interval1;
+        const diff = interval2 - interval1 - (shouldReduce ? 1 : 0);
+        shouldReduce = false;
 
-        if (diff > 0) {
+        if (diff >= 0) {
             response.push({ type: label, value: diff });
         } else {
             if (label == DAY_OF_MONTH) throw Error('We regret to inform it’s not possible to calculate this value for a day of a month.');
 
             const [min, max] = range(label);
             response.push({ type: label, value: max + 1 - min + diff });
+            shouldReduce = true;
         }
     }
 
--- src/cron-diff.spec.js
+++ src/cron-diff.spec.js
@@ -1,7 +1,9 @@
 const cronDiff = require('./cron-diff');
 
-describe('cron-diff', () => {
-    describe('simple tests, with one value', () => {
+describe('cronDiff(..)', () => {
+    // describe('should reject handling mismatched crons');
+
+    describe('for crons with one value,', () => {
         it('should work with a positive diff of minutes', () => {
             const actual = cronDiff('10 * * * *', '15 * * * *');
             const expected = [{
@@ -55,7 +57,7 @@ describe('cron-diff', () => {
         it('should throw an error for a negative diff of days of a month', () => {
             const actual =
 
-            expect(() => { cronDiff('* * 11 * *', '* * 10 * *'); }).toThrowError();
+                expect(() => { cronDiff('* * 11 * *', '* * 10 * *'); }).toThrowError();
         });
 
         it('should work with a positive diff of months', () => {
@@ -108,4 +110,72 @@ describe('cron-diff', () => {
             expect(actual).toMatchObject(expected);
         });
     });
+
+    describe('for crons with hours and minutes,', () => {
+        it('should work with both positive diffs of minutes and hours', () => {
+            const actual = cronDiff('10 3 * * *', '15 4 * * *');
+            const expected = [
+                {
+                    type: 'minute',
+                    value: 5,
+                },
+                {
+                    type: 'hour',
+                    value: 1,
+                },
+            ];
+
+            expect(actual).toMatchObject(expected);
+        });
+
+        it('should work with a negative diff of minutes and a positive diff of hours', () => {
+            const actual = cronDiff('20 3 * * *', '15 4 * * *');
+            const expected = [
+                {
+                    type: 'minute',
+                    value: 55,
+                },
+                {
+                    type: 'hour',
+                    value: 0,
+                },
+            ];
+
+            expect(actual).toMatchObject(expected);
+        });
+
+        it('should work with a positive diff of minutes and a negative diff of hours', () => {
+            const actual = cronDiff('10 4 * * *', '15 3 * * *');
+            const expected = [
+                {
+                    type: 'minute',
+                    value: 5,
+                },
+                {
+                    type: 'hour',
+                    value: 23,
+                },
+            ];
+
+            expect(actual).toMatchObject(expected);
+        });
+
+        it('should work with a negative diff of minutes and a negative diff of hours', () => {
+            const actual = cronDiff('20 4 * * *', '15 3 * * *');
+            const expected = [
+                {
+                    type: 'minute',
+                    value: 55,
+                },
+                {
+                    type: 'hour',
+                    value: 22,
+                },
+            ];
+
+            expect(actual).toMatchObject(expected);
+        });
+    });
+
+    // describe('should return object with 0 value if there were values and skip if had stars');
 });
